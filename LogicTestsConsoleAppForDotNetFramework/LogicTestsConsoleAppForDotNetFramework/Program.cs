00d196b5f2b972b6101ce548c6b7d8b8377d59c0
AnkitSahay <ankitsahay1990@gmail.com>
2023-01-23 23:55:22 +0530

    Added Star Pinting with Triangle 1,3,5,7,9.....

--- LogicTestsConsoleAppForDotNetFramework/LogicTestsConsoleAppForDotNetFramework/Program.cs
+++ LogicTestsConsoleAppForDotNetFramework/LogicTestsConsoleAppForDotNetFramework/Program.cs
@@ -6,9 +6,10 @@ namespace LogicTestsConsoleAppForDotNetFramework
     {
         static void Main(string[] args)
         {
-            CountRepeatedCharactersInString.ShowCharCount(@"We use first- and third party cookies to improve the site, to enhance your experience and to offer you website personalization features, such as an Orientation filter. You agree to all our cookies by clicking on 'Accept All Cookies' or by saving your changes in Cookie Settings. Read more about our use of cookies in our");
-            Console.WriteLine(SortCsvByColumn.Sort(null));
-            Console.WriteLine("\n Press Enter to CLOSE");
+            StarPrintTringle.PrintStar();
+            //CountRepeatedCharactersInString.ShowCharCount(@"We use first- and third party cookies to improve the site, to enhance your experience and to offer you website personalization features, such as an Orientation filter. You agree to all our cookies by clicking on 'Accept All Cookies' or by saving your changes in Cookie Settings. Read more about our use of cookies in our");
+            //Console.WriteLine(SortCsvByColumn.Sort(null));
+            //Console.WriteLine("\n Press Enter to CLOSE");
             Console.Read();
         }
     }
---
+++ LogicTestsConsoleAppForDotNetFramework/LogicTestsConsoleAppForDotNetFramework/StarPrintTringle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicTestsConsoleAppForDotNetFramework
+{
+    public static class StarPrintTringle
+    {
+        public static void PrintStar(int n = 5)
+        {
+            //       *
+            //      ***
+            //     *****
+            //    *******
+            //   *********
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j < (n - i); j++)
+                    Console.Write(" ");
+                var starsCount = 0; 
+                do
+                {
+                    Console.Write("*");
+                    starsCount++;
+                } 
+                while (starsCount < (i * 2) - 1);
+                Console.WriteLine();
+            }
+        }
+    }
+}
