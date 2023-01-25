52ca6224a882698bf0bf6094705246edb09bac6e
AnkitSahay <ankitsahay1990@gmail.com>
2023-01-26 03:59:45 +0530

    Added Int Array Shorting Logic with swipe without 3rd variable.

---
+++ LogicTestsConsoleAppForDotNetFramework/LogicTestsConsoleAppForDotNetFramework/ArraySorting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicTestsConsoleAppForDotNetFramework
+{
+    public static class ArraySorting
+    {
+        public static void SortMyArray(int[] intArray)
+        {
+            if (intArray == null || intArray.Length <= 0)
+                intArray = new int[] { 9, 2, 4, 3, 1, 5 };
+
+            //Array.Sort(intArray);
+            //Array.Reverse(intArray);
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                var smallest = intArray[i];
+                for (int j = i + 1; j < intArray.Length ; j++)
+                {
+                    if (intArray[i] > intArray[j])
+                    {
+                        intArray[i] = intArray[i] + intArray[j]; // i=10, j=20 |  10 + 20  i=30
+                        intArray[j] = intArray[i] - intArray[j]; // i=30, j=20 |  30 - 20  j=10
+                        intArray[i] = intArray[i] - intArray[j]; // i=30, j=10 |  30 - 10  i=20
+                    }
+                }
+            }
+            foreach (int i in intArray)
+                Console.WriteLine(i);
+            Console.ReadLine();
+        }
+
+    }
+}
--- LogicTestsConsoleAppForDotNetFramework/LogicTestsConsoleAppForDotNetFramework/Program.cs
+++ LogicTestsConsoleAppForDotNetFramework/LogicTestsConsoleAppForDotNetFramework/Program.cs
@@ -6,7 +6,8 @@ namespace LogicTestsConsoleAppForDotNetFramework
     {
         static void Main(string[] args)
         {
-            StarPrintTringle.PrintStar();
+          ArraySorting.SortMyArray(null);
+//            StarPrintTringle.PrintStar();
             //CountRepeatedCharactersInString.ShowCharCount(@"We use first- and third party cookies to improve the site, to enhance your experience and to offer you website personalization features, such as an Orientation filter. You agree to all our cookies by clicking on 'Accept All Cookies' or by saving your changes in Cookie Settings. Read more about our use of cookies in our");
             //Console.WriteLine(SortCsvByColumn.Sort(null));
             //Console.WriteLine("\n Press Enter to CLOSE");
